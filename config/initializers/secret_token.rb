19d5aad6ad7c04a179845229c288fbce225970e3
Eduardo Bohrer <nbluisrs@gmail.com>
2011-06-02 20:18:43 -0300

    rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Convite::Application.config.secret_token = '3825295f52181dd3a6b877843364f35b1125d11c914567a31cd4268f3ab322fc849004525d15a036de816233346b3f2d1e038d94c3309ad52f1e5fc6da9f248a'
